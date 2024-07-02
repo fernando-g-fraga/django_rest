f1439eeecbf02516ebc8999c44b88fa0e1777921
Fernando <fernandogrfraga@gmail.com>
2024-07-01 23:54:07 -0300

    created serialized alunos and cursos data

---
+++ escola/serializer.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from escola.models import Aluno,Curso
+
+class Aluno_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno 
+        fields = ['id','nome','rg','cpf','data_nascimento']
+
+
+class Curso_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = ['__all__']
+
+
+
