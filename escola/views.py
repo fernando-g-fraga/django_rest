817d7d50d731b90c964f64d805a313da69060946
Fernando <fernandogrfraga@gmail.com>
2024-07-02 23:16:33 -0300

    added permissions and basic auth on created routes

--- escola/views.py
+++ escola/views.py
@@ -1,23 +1,32 @@
 from rest_framework import viewsets,generics
 from escola.models import Aluno,Curso, Matricula
 from escola.serializer import Aluno_Serializer, Curso_Serializer, Matricula_Serializer, ListaMatriculasAlunoSerializer,ListaAlunosMatriculadosSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated 
 
 class Alunos_viewset(viewsets.ModelViewSet):
     """Exibir todos os alunos cadastrados"""
 
     queryset = Aluno.objects.all()
     serializer_class = Aluno_Serializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    
 
 class Cursos_viewset(viewsets.ModelViewSet):
     """Exibir todos os cursos cadastrados"""
 
     queryset = Curso.objects.all()
     serializer_class = Curso_Serializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class Matriculas_viewset(viewsets.ModelViewSet):
 
     queryset = Matricula.objects.all()
     serializer_class = Matricula_Serializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class ListaMatriculasAluno(generics.ListAPIView):
     '''Listando Matriculas do Aluno'''
@@ -25,6 +34,8 @@ class ListaMatriculasAluno(generics.ListAPIView):
         query_set = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
         return query_set
     serializer_class = ListaMatriculasAlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class ListaAlunosMatriculados (generics.ListAPIView):
     '''Listando Alunos Matriculados no curso'''
@@ -34,5 +45,7 @@ class ListaAlunosMatriculados (generics.ListAPIView):
         
         return query_set
     serializer_class = ListaAlunosMatriculadosSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
